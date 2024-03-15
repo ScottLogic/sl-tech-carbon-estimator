a6bb73fc1ca85d1a5fd2406ca50a6f975c0d5166
Jareth Main <jmain@scottlogic.com>
2024-03-15 14:12:45 +0000

    Update toplevel control and index

--- src/app/carbon-estimator/carbon-estimator.component.ts
+++ src/app/carbon-estimator/carbon-estimator.component.ts
@@ -1,33 +1,33 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { CarbonEstimatorFormComponent } from '../carbon-estimator-form/carbon-estimator-form.component';
 import { CarbonEstimationComponent } from '../carbon-estimation/carbon-estimation.component';
 import { CarbonEstimation, EstimatorValues } from '../carbon-estimator';
 import { CarbonEstimationService } from '../services/carbon-estimation.service';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'sl-carbon-estimator',
   standalone: true,
-  imports: [CarbonEstimatorFormComponent, CarbonEstimationComponent, FormsModule],
+  imports: [CarbonEstimatorFormComponent, CarbonEstimationComponent, FormsModule, CommonModule],
   templateUrl: './carbon-estimator.component.html',
-  styles: [':host {display: flex}'],
 })
 export class CarbonEstimatorComponent {
-  @Input()
-  public sideBySide: boolean = false;
-  public view: 'form' | 'calculation' = 'form';
+  public showEstimation = true;
   public formValue: EstimatorValues | undefined;
-  public carbonEstimation: CarbonEstimation = {} as CarbonEstimation;
+  public carbonEstimation: CarbonEstimation = {
+    version: '0.0.1',
+    upstreamEmissions: 25,
+    cloudEmissions: 25,
+    directEmissions: 25,
+    downstreamEmissions: 25,
+  };
 
   constructor(private estimationService: CarbonEstimationService) {}
 
   public handleFormSubmit(formValue: EstimatorValues) {
     this.formValue = formValue;
     this.carbonEstimation = this.estimationService.calculateCarbonEstimation(this.formValue);
-    this.view = 'calculation';
-  }
-
-  public handleBack() {
-    this.view = 'form';
+    this.showEstimation = true;
   }
 }
