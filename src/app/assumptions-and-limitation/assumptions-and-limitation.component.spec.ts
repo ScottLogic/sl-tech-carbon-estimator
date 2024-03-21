080f17e41749fe4cad9cb996b62d78715404d920
Jareth Main <jmain@scottlogic.com>
2024-03-21 11:13:38 +0000

    Add unit test to assumpeitons compinent

--- src/app/assumptions-and-limitation/assumptions-and-limitation.component.spec.ts
+++ src/app/assumptions-and-limitation/assumptions-and-limitation.component.spec.ts
@@ -8,16 +8,27 @@ describe('AssumptionsAndLimitationComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AssumptionsAndLimitationComponent]
-    })
-    .compileComponents();
-    
+      imports: [AssumptionsAndLimitationComponent],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(AssumptionsAndLimitationComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should emit close event when click close button', () => {
+    spyOn(component.closeEvent, 'emit');
+
+    fixture.nativeElement.querySelector('button').click();
+
+    expect(component.closeEvent.emit).toHaveBeenCalledTimes(1);
+  });
+
+  it('should emit close event when press esc key function ran', () => {
+    spyOn(component.closeEvent, 'emit');
+
+    component.onEscKeydown();
+
+    expect(component.closeEvent.emit).toHaveBeenCalledTimes(1);
   });
 });
