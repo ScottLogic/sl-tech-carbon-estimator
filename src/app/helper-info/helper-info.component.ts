64e75976c9b29be497c5728b68cf2b0561730249
Jareth Main <jmain@scottlogic.com>
2024-04-04 14:32:11 +0100

    Create helper component, and make icon bigger

---
+++ src/app/helper-info/helper-info.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { SatPopoverModule } from '@ncstate/sat-popover';
+
+@Component({
+  selector: 'sl-helper-info',
+  standalone: true,
+  imports: [SatPopoverModule],
+  templateUrl: './helper-info.component.html',
+})
+export class HelperInfoComponent {}
