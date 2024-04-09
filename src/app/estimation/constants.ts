4343ca777e60b9f136f61e24de07567335dc4180
Matthew Griffin <mgriffin@scottlogic.com>
2024-04-09 14:23:24 +0100

    Apply on-premise PUE factor to server and network energy amounts

---
+++ src/app/estimation/constants.ts
@@ -0,0 +1,2 @@
+// Source for on-premise data centres: https://www.statista.com/statistics/1229367/data-center-average-annual-pue-worldwide/
+export const ON_PREMISE_AVERAGE_PUE = 1.58;
--- src/app/estimation/estimate-direct-emissions.ts
+++ src/app/estimation/estimate-direct-emissions.ts
@@ -1,12 +1,13 @@
 import { DeviceCounts, DirectEstimation, WorldLocation } from '../carbon-estimator';
+import { ON_PREMISE_AVERAGE_PUE } from './constants';
 import { desktop, laptop, network, server } from './device-type';
 import { estimateEnergyEmissions } from './estimate-energy-emissions';
 
 export function estimateDirectEmissions(deviceCounts: DeviceCounts, onPremLocation: WorldLocation): DirectEstimation {
   const desktopEnergy = desktop.estimateYearlyEnergy(deviceCounts.desktopCount);
   const laptopEnergy = laptop.estimateYearlyEnergy(deviceCounts.laptopCount);
-  const serverEnergy = server.estimateYearlyEnergy(deviceCounts.serverCount);
-  const networkEnergy = network.estimateYearlyEnergy(deviceCounts.networkCount);
+  const serverEnergy = server.estimateYearlyEnergy(deviceCounts.serverCount) * ON_PREMISE_AVERAGE_PUE;
+  const networkEnergy = network.estimateYearlyEnergy(deviceCounts.networkCount) * ON_PREMISE_AVERAGE_PUE;
 
   const desktopDirectEmissions = estimateEnergyEmissions(desktopEnergy, onPremLocation);
   const laptopDirectEmissions = estimateEnergyEmissions(laptopEnergy, onPremLocation);
