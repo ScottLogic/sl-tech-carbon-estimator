19a7e34ca1d4a6422356c933d7d78faaa2cf3e51
Jareth Main <jmain@scottlogic.com>
2024-04-12 11:49:21 +0100

    When click on assumption button focus on shown assumptions text. W

--- src/app/assumptions-and-limitation/assumptions-and-limitation.component.ts
+++ src/app/assumptions-and-limitation/assumptions-and-limitation.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, HostListener, Output } from '@angular/core';
+import { AfterContentInit, Component, ElementRef, EventEmitter, HostListener, Output, ViewChild } from '@angular/core';
 import { CLOUD_AVERAGE_PUE, ON_PREMISE_AVERAGE_PUE } from '../estimation/constants';
 
 @Component({
@@ -6,10 +6,16 @@ import { CLOUD_AVERAGE_PUE, ON_PREMISE_AVERAGE_PUE } from '../estimation/constan
   standalone: true,
   templateUrl: './assumptions-and-limitation.component.html',
 })
-export class AssumptionsAndLimitationComponent {
+export class AssumptionsAndLimitationComponent implements AfterContentInit {
   @Output() public closeEvent = new EventEmitter<void>();
   readonly ON_PREMISE_AVERAGE_PUE = ON_PREMISE_AVERAGE_PUE;
   readonly CLOUD_AVERAGE_PUE = CLOUD_AVERAGE_PUE;
+
+  @ViewChild('assumptionsLimitation', { static: true }) public assumptionsLimitation!: ElementRef;
+
+  public ngAfterContentInit(): void {
+    this.assumptionsLimitation.nativeElement.focus();
+  }
 
   public onClose(): void {
     this.closeEvent.emit();
