46dbc9b01864c52a27e965a76897f82e4ad055a6
Jareth Main <jmain@scottlogic.com>
2024-04-22 17:05:17 +0100

    Add publish and post-publish scripts and fix unit tests

---
+++ scripts/github-release.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Ran after the build is published and version and change log have been merged into the main branch
+
+version=$(npm pkg get version --workspaces=false | tr -d \")
+# Remove --pre-release flag when estimator is ready for production
+gh release create v$version ./dist/tech-carbon-estimaotr/*.tgz --pre-release
---
+++ scripts/publish.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Check if npm is logged in
+if ! npm whoami &> /dev/null; then
+  echo "Please log in to npm before running this script, run npm login"
+  exit 1
+fi
+
+# Get version bump
+bump=patch
+if [ -z "$1" ]; then
+  echo "No version bump type provided, defaulting to patch"
+elif [ "$1" = "major" ] || [ "$1" = "minor" ] || [ "$1" = "patch" ]; then
+  bump= $1
+else
+  echo "Invalid version bump type provided, specfiy one of major, minor, or patch"
+  exit 1
+fi
+
+# Remove current dist folder
+rm -rf /dist/tech-carbon-estimator
+
+# Build and test
+npm run build
+
+npm run test
+
+# Update version and commit version bump and tag
+npm version $bump
+
+version=$(npm pkg get version --workspaces=false | tr -d \")
+
+# Publish to npm
+cp README.md LICENSE package.json ./dist/tech-carbon-estimator/
+
+cd dist/tech-carbon-estimator
+
+npm pkg delete scripts private devDependencies
+
+npm pkg set main=main.js
+
+npm pack
+
+publishFile=tech-carbon-estimator-"${version}".tgz
+
+npm publish $publishFile
+
+echo "Published version $version to npm, run git push --follow-tags to push version update to git"
