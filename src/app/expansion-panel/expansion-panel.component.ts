33097ed0d50d736b51cbc8f6c6267aeab1fb849d
jantoun-scottlogic <jantoun@scottlogic.com>
2024-05-09 09:18:01 +0100

    Make tooltips a toggle option

--- src/app/expansion-panel/expansion-panel.component.ts
+++ src/app/expansion-panel/expansion-panel.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, EventEmitter, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ExpansionPanelButtonConfig, defaultButtonConfig, helperButtonConfig } from './expansion-panel.constants';
 
 @Component({
   selector: 'expansion-panel',
@@ -8,12 +9,21 @@ import { ChangeDetectionStrategy, Component, EventEmitter, Output } from '@angul
   templateUrl: './expansion-panel.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ExpansionPanelComponent {
+export class ExpansionPanelComponent implements OnInit {
+  @Input() helperPanel? = false;
   @Output() expandedChanged = new EventEmitter<void>();
   public expanded: boolean = true;
+  public buttonConfig: ExpansionPanelButtonConfig = defaultButtonConfig;
 
   toggle() {
     this.expanded = !this.expanded;
     this.expandedChanged.emit();
+  }
+
+  ngOnInit(): void {
+    if (this.helperPanel) {
+      this.expanded = false;
+      this.buttonConfig = helperButtonConfig;
+    }
   }
 }
---
+++ src/app/expansion-panel/expansion-panel.constants.ts
@@ -0,0 +1,17 @@
+export type ExpansionPanelButtonConfig = {
+  expandedIcon: string;
+  collapsedIcon: string;
+  classAttribute: string;
+};
+
+export const defaultButtonConfig = {
+  expandedIcon: 'expand_less',
+  collapsedIcon: 'expand_more',
+  classAttribute: 'material-icons-outlined tce-text tce-text-slate-600 hover:tce-bg-slate-200 hover:tce-rounded',
+};
+
+export const helperButtonConfig = {
+  expandedIcon: 'help_outline',
+  collapsedIcon: 'help_outline',
+  classAttribute: 'material-icons-outlined tce-text-base tce-text-slate-600 hover:tce-bg-slate-300 tce-rounded',
+};
