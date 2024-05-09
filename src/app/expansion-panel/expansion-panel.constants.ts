b42737dcd28bf65fc052914fe5f9d1f860cb52d4
jantoun-scottlogic <jantoun@scottlogic.com>
2024-05-09 09:34:02 +0100

    Add explicit type to button configs

--- src/app/expansion-panel/expansion-panel.constants.ts
+++ src/app/expansion-panel/expansion-panel.constants.ts
@@ -4,13 +4,13 @@ export type ExpansionPanelButtonConfig = {
   classAttribute: string;
 };
 
-export const defaultButtonConfig = {
+export const defaultButtonConfig: ExpansionPanelButtonConfig = {
   expandedIcon: 'expand_less',
   collapsedIcon: 'expand_more',
   classAttribute: 'material-icons-outlined tce-text tce-text-slate-600 hover:tce-bg-slate-200 hover:tce-rounded',
 };
 
-export const helperButtonConfig = {
+export const helperButtonConfig: ExpansionPanelButtonConfig = {
   expandedIcon: 'help_outline',
   collapsedIcon: 'help_outline',
   classAttribute: 'material-icons-outlined tce-text-base tce-text-slate-600 hover:tce-bg-slate-300 tce-rounded',
