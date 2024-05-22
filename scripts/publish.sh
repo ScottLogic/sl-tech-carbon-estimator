5a7df0b1fde4151faa26416e02820a7f31041810
Matthew Griffin <mgriffin@scottlogic.com>
2024-05-22 15:27:50 +0100

    Remove files specification when publishing entire folder

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -48,7 +48,7 @@ cp README.md package.json ./dist/tech-carbon-estimator/
 
 cd dist/tech-carbon-estimator
 
-npm pkg delete scripts private devDependencies
+npm pkg delete scripts private devDependencies files
 
 npm pkg set main=main.js
 
