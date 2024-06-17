58d2f7a4c976a4edced32e4445a27d3830dcf272
Matthew Griffin <mgriffin@scottlogic.com>
2024-06-17 11:43:29 +0100

    Fix up a couple more errors

--- playwright/tests/test_0_TrialTest_HappyPath.py
+++ playwright/tests/test_0_TrialTest_HappyPath.py
@@ -15,27 +15,23 @@ def test_example(page: Page) -> None:
     page.get_by_text("Laptops 50%").click()
     expect(page.get_by_role("heading", name="On-Premise Servers")).to_be_visible()
     expect(page.get_by_label("Number of Servers:")).to_have_value("10");
-    expect(page.get_by_label("Where are they primarily")).to_have_value("global");
+    expect(page.get_by_label("Where are they primarily")).to_have_value("WORLD");
     expect(page.get_by_role("heading", name="Cloud Services")).to_be_visible()
     expect(page.get_by_text("Cloud 50%")).to_be_visible()
-    expect(page.get_by_text("On-prem 50%")).to_be_visible()
-    expect(page.get_by_label("Where are your cloud servers")).to_have_value("global");
+    expect(page.get_by_text("On-premise 50%")).to_be_visible()
+    expect(page.get_by_label("Where are your cloud servers")).to_have_value("WORLD");
     expect(page.get_by_label("What is your monthly cloud")).to_have_value("0: Object");
     expect(page.get_by_role("heading", name="Users")).to_be_visible()
-    expect(page.get_by_text("Where are your users")).to_be_visible()
-    expect(page.get_by_label("Where are your users")).to_have_value("global");
+    expect(page.get_by_text("Where are your end-users")).to_be_visible()
+    expect(page.get_by_label("Where are your end-users")).to_have_value("WORLD");
     expect(page.get_by_text("How many monthly active users")).to_be_visible()
     expect(page.get_by_label("How many monthly active users")).to_have_value("100");
     expect(page.get_by_text("Mobile 50%")).to_be_visible()
     expect(page.get_by_text("Computer 50%")).to_be_visible()
-    expect(page.get_by_text("What's the purpose of your")).to_be_visible()
-    expect(page.get_by_label("What's the purpose of your")).to_have_value("average");
+    expect(page.get_by_text("What's the primary purpose of your")).to_be_visible()
+    expect(page.get_by_label("What's the primary purpose of your")).to_have_value("average");
     page.get_by_role("button", name="Calculate").click()
-    expect(page.get_by_text("Upstream Emissions:")).to_be_visible()
-    expect(page.get_by_text("28%")).to_be_visible()
-    expect(page.get_by_text("Indirect Emissions:")).to_be_visible()
-    expect(page.get_by_text("0%", exact=True)).to_be_visible()
-    expect(page.get_by_text("Direct Emissions:", exact=True)).to_be_visible()
-    expect(page.get_by_text("71%")).to_be_visible()
-    expect(page.get_by_text("Downstream Emissions:")).to_be_visible()
-    expect(page.get_by_text("1%", exact=True)).to_be_visible()
+    expect(page.locator("foreignobject")).to_contain_text("Upstream Emissions - 33%")
+    expect(page.locator("foreignobject")).to_contain_text("Direct Emissions - 65%")
+    expect(page.locator("foreignobject")).to_contain_text("Indirect Emissions - 1%")
+    expect(page.locator("foreignobject")).to_contain_text("Downstream Emissions - <1%")
