1df89c1e5867e0dd3a339ea55b32c816a17099e4
Matthew Griffin <mgriffin@scottlogic.com>
2024-06-21 12:08:20 +0100

    Support Dark mode Remove specified text colour from most elements so that a page wide dark setting can override it Specify input and select text colour in dark mode due to light background Specify diagram tooltip colour in dark mode due to light background Specify diagram legend colour in dark mode due to dark background

--- src/app/carbon-estimator-form/carbon-estimator-form.constants.ts
+++ src/app/carbon-estimator-form/carbon-estimator-form.constants.ts
@@ -94,6 +94,6 @@ export const formContext = {
 
 export const questionPanelConfig: ExpansionPanelConfig = {
   startsExpanded: false,
-  buttonStyles: 'material-icons-outlined tce-text-base tce-text-slate-600 hover:tce-bg-slate-200 hover:tce-rounded',
+  buttonStyles: 'material-icons-outlined tce-text-base hover:tce-bg-slate-200 hover:tce-rounded',
   contentContainerStyles: 'tce-px-3 tce-py-2 tce-bg-slate-100 tce-border tce-border-slate-400 tce-rounded tce-text-sm',
 };
--- src/app/expansion-panel/expansion-panel.constants.ts
+++ src/app/expansion-panel/expansion-panel.constants.ts
@@ -6,6 +6,6 @@ export type ExpansionPanelConfig = {
 
 export const defaultConfig: ExpansionPanelConfig = {
   startsExpanded: true,
-  buttonStyles: 'material-icons-outlined tce-text tce-text-slate-600 hover:tce-bg-slate-200 hover:tce-rounded',
+  buttonStyles: 'material-icons-outlined tce-text hover:tce-bg-slate-200 hover:tce-rounded',
   contentContainerStyles: '',
 };
