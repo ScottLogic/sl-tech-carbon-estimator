4f5d19ffdd337f2ca0aa44b6bb5d04ebf7162bbf
jantoun-scottlogic <jantoun@scottlogic.com>
2024-06-27 16:44:29 +0100

    Add invalid-after-interaction pipe

---
+++ src/app/pipes/invalid-after-interaction.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { InvalidAfterInteractionPipe } from './invalid-after-interaction.pipe';
+
+describe('InvalidAfterInteractionPipe', () => {
+  it('create an instance', () => {
+    const pipe = new InvalidAfterInteractionPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
