2943a0f7274eb450f7db9fb871de6dbf25fd3f2c
jantoun-scottlogic <jantoun@scottlogic.com>
2024-07-01 12:50:13 +0100

    Add unit tests for pipe

--- src/app/pipes/invalid-and-interaction.pipe.spec.ts
+++ src/app/pipes/invalid-and-interaction.pipe.spec.ts
@@ -1,8 +1,45 @@
+import { FormControl, Validators } from '@angular/forms';
 import { InvalidAndInteractionPipe } from './invalid-and-interaction.pipe';
 
 describe('InvalidAndInteractionPipe', () => {
   it('create an instance', () => {
     const pipe = new InvalidAndInteractionPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('should return false if the control is valid and there has been no interaction', () => {
+    const formControl = new FormControl('hello', Validators.required);
+    const pipe = new InvalidAndInteractionPipe();
+
+    const transformed = pipe.transform(formControl);
+    expect(transformed).toBeFalse();
+  });
+
+  it('should return false if the control is valid and there has been interaction', () => {
+    const formControl = new FormControl('hello', Validators.required);
+    const pipe = new InvalidAndInteractionPipe();
+    formControl.markAsDirty();
+    formControl.markAsTouched();
+
+    const transformed = pipe.transform(formControl);
+    expect(transformed).toBeFalse();
+  });
+
+  it('should return false if the control is invalid and there has been no interaction', () => {
+    const formControl = new FormControl('', Validators.required);
+    const pipe = new InvalidAndInteractionPipe();
+
+    const transformed = pipe.transform(formControl);
+    expect(transformed).toBeFalse();
+  });
+
+  it('should return true if the form is invalid and there has been interaction', () => {
+    const formControl = new FormControl('', Validators.required);
+    const pipe = new InvalidAndInteractionPipe();
+    formControl.markAsDirty();
+    formControl.markAsTouched();
+
+    const transformed = pipe.transform(formControl);
+    expect(transformed).toBeTrue();
   });
 });
