5afdc9b62b22d608bad10f8b66027655148325c2
Jareth Main <jmain@scottlogic.com>
2024-07-18 14:46:29 +0100

    Style tabs

--- src/app/tab/tab-item/tab-item.component.ts
+++ src/app/tab/tab-item/tab-item.component.ts
@@ -5,7 +5,6 @@ import { Component, input, model } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './tab-item.component.html',
-  styleUrl: './tab-item.component.css',
 })
 export class TabItemComponent {
   public active = model(false);
