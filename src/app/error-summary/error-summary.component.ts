5e8eff48972a2e333ab6c15b468d5967daf7258d
jantoun-scottlogic <jantoun@scottlogic.com>
2024-07-25 11:19:41 +0100

    Delete empty css file

--- src/app/error-summary/error-summary.component.ts
+++ src/app/error-summary/error-summary.component.ts
@@ -6,7 +6,6 @@ import { ValidationError } from '../carbon-estimator-form/carbon-estimator-form.
   standalone: true,
   imports: [],
   templateUrl: './error-summary.component.html',
-  styleUrl: './error-summary.component.css',
 })
 export class ErrorSummaryComponent {
   validationErrors = input.required<ValidationError[]>();
