803024f16177cdb42cc532e3b43ea59ce2f95f8a
jantoun-scottlogic <jantoun@scottlogic.com>
2024-07-25 11:01:02 +0100

    Add unit test

--- src/app/error-summary/error-summary.component.spec.ts
+++ src/app/error-summary/error-summary.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ErrorSummaryComponent } from './error-summary.component';
+import { ValidationError } from '../carbon-estimator-form/carbon-estimator-form.component';
 
 describe('ErrorSummaryComponent', () => {
   let component: ErrorSummaryComponent;
@@ -13,10 +14,29 @@ describe('ErrorSummaryComponent', () => {
 
     fixture = TestBed.createComponent(ErrorSummaryComponent);
     component = fixture.componentInstance;
+
+    const validationErrors: ValidationError[] = [
+      {
+        inputId: 'input1',
+        errorMessage: 'Input 1 must be greater than 0',
+      },
+      {
+        inputId: 'input2',
+        errorMessage: 'Input 2 must be greater than 0',
+      },
+    ];
+
+    fixture.componentRef.setInput('validationErrors', validationErrors);
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display validation error messages', () => {
+    expect(fixture.nativeElement.textContent).toContain('Input 1 must be greater than 0');
+    expect(fixture.nativeElement.textContent).toContain('Input 2 must be greater than 0');
   });
 });
