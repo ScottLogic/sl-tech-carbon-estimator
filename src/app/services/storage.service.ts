9911d9d8d292f357ee2642b815161c1085f4157d
jantoun-scottlogic <jantoun@scottlogic.com>
2024-08-05 14:48:34 +0100

    Add explicit return types to storage service methods

--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -11,11 +11,11 @@ export const BROWSER_STORAGE = new InjectionToken<Storage>('Browser Storage', {
 export class StorageService {
   constructor(@Inject(BROWSER_STORAGE) public storage: Storage) {}
 
-  get(key: string) {
+  get(key: string): string | null {
     return this.storage.getItem(key);
   }
 
-  set(key: string, value: string) {
+  set(key: string, value: string): void {
     return this.storage.setItem(key, value);
   }
 }
