f2cfaa34000fdf308c6a270492a851fb4d8f7139
jantoun-scottlogic <110816538+jantoun-scottlogic@users.noreply.github.com>
2024-08-05 08:52:09 +0100

    SFD-128: Add error summary for validation errors (#118)
    * Add error summary component
    
    * Move validation error messages to variable and add getter for errors
    
    * Show error summary on submission of invalid form
    
    * Enable submit button when form invalid
    
    If the form is invalid the error summary will be shown.
    
    * Focus error summary on invalid submission
    
    * Do not update error summary until resubmission
    
    * Style error summary
    
    * Remove full stops from error messages
    
    * Refactor error config
    
    * Add dark mode styles
    
    * Remove extra monthly active users inline error message
    
    * Remove explicit typing from errorConfig
    
    * Add unit test
    
    * Refactor handleSubmit
    
    * Delete empty css file
    
    * Add class to center error text in error flex boxes
    
    * Move type and constants to constants.ts

---
+++ src/app/error-summary/error-summary.component.ts
@@ -0,0 +1,13 @@
+import { Component, ElementRef, input, ViewChild } from '@angular/core';
+import { ValidationError } from '../carbon-estimator-form/carbon-estimator-form.constants';
+
+@Component({
+  selector: 'error-summary',
+  standalone: true,
+  imports: [],
+  templateUrl: './error-summary.component.html',
+})
+export class ErrorSummaryComponent {
+  validationErrors = input.required<ValidationError[]>();
+  @ViewChild('errorSummary') summary!: ElementRef<HTMLDivElement>;
+}
