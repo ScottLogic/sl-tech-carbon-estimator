03ea38d776e9322179e4665dc472624a12d268a5
Matthew Griffin <mgriffin@scottlogic.com>
2024-08-09 15:57:06 +0100

    Remove unused behaviour from assumptions component

--- src/app/assumptions-and-limitation/assumptions-and-limitation.component.ts
+++ src/app/assumptions-and-limitation/assumptions-and-limitation.component.ts
@@ -1,4 +1,4 @@
-import { AfterContentInit, Component, ElementRef, EventEmitter, HostListener, Output, ViewChild } from '@angular/core';
+import { AfterContentInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { CLOUD_AVERAGE_PUE, ON_PREMISE_AVERAGE_PUE } from '../estimation/constants';
 import { siteTypeInfo } from '../estimation/estimate-downstream-emissions';
 import { PurposeOfSite, WorldLocation, locationArray, purposeOfSiteArray } from '../types/carbon-estimator';
@@ -33,7 +33,6 @@ const locationDescriptions: Record<WorldLocation, string> = {
   imports: [DecimalPipe, ExternalLinkDirective],
 })
 export class AssumptionsAndLimitationComponent implements AfterContentInit {
-  @Output() public closeEvent = new EventEmitter<boolean>();
   readonly ON_PREMISE_AVERAGE_PUE = ON_PREMISE_AVERAGE_PUE;
   readonly CLOUD_AVERAGE_PUE = CLOUD_AVERAGE_PUE;
   readonly siteTypeInfo = purposeOfSiteArray.map(purpose => ({
@@ -84,15 +83,5 @@ export class AssumptionsAndLimitationComponent implements AfterContentInit {
 
   public ngAfterContentInit(): void {
     this.assumptionsLimitation.nativeElement.focus({ preventScroll: true });
-  }
-
-  public onClose(hasFocus = true): void {
-    this.closeEvent.emit(hasFocus);
-  }
-
-  @HostListener('document:keydown.escape', ['$event'])
-  public onEscKeydown(): void {
-    const hasFocus = this.assumptionsLimitation.nativeElement.contains(document.activeElement);
-    this.onClose(hasFocus);
   }
 }
