44f87aa2f1748a68adc5ce1edf52b23652cc1345
jantoun-scottlogic <jantoun@scottlogic.com>
2024-10-03 09:48:05 +0100

    Implement right arrow key tab switching

--- src/app/tab/tabs/tabs.component.ts
+++ src/app/tab/tabs/tabs.component.ts
@@ -1,4 +1,4 @@
-import { AfterContentInit, Component, ContentChildren, QueryList } from '@angular/core';
+import { AfterContentInit, Component, ContentChildren, ElementRef, QueryList, ViewChildren } from '@angular/core';
 import { TabItemComponent } from '../tab-item/tab-item.component';
 
 @Component({
@@ -9,6 +9,7 @@ import { TabItemComponent } from '../tab-item/tab-item.component';
 })
 export class TabsComponent implements AfterContentInit {
   @ContentChildren(TabItemComponent) tabs!: QueryList<TabItemComponent>;
+  @ViewChildren('tabButton') tabButtons!: QueryList<ElementRef<HTMLButtonElement>>;
 
   public ngAfterContentInit() {
     const activeTabs = this.tabs.filter(tab => tab.active());
@@ -25,10 +26,27 @@ export class TabsComponent implements AfterContentInit {
     selectedTab.active.set(true);
   }
 
-  public onKeydown(event: KeyboardEvent) {
+  selectAndFocusTab(selectedTabIndex: number) {
+    const newTab = this.tabs.get(selectedTabIndex)!;
+    const newTabButton = this.tabButtons.get(selectedTabIndex)!;
+    this.selectTab(newTab);
+    newTabButton.nativeElement.focus();
+  }
+
+  setSelectedToNextTab(currentTabIndex: number) {
+    let newTabIndex: number;
+    if (currentTabIndex === this.tabs.length - 1) {
+      newTabIndex = 0;
+    } else {
+      newTabIndex = currentTabIndex + 1;
+    }
+    this.selectAndFocusTab(newTabIndex);
+  }
+
+  public onKeydown(event: KeyboardEvent, currentIndex: number) {
     switch (event.key) {
       case 'ArrowRight':
-        console.log('right');
+        this.setSelectedToNextTab(currentIndex);
         break;
 
       case 'ArrowLeft':
