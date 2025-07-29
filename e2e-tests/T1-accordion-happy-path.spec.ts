b94f8ac7c6185b4585d2ddc6e682a095f2fca8a3
Maxwell Nyamunda <142179888+mnyamunda-scottlogic@users.noreply.github.com>
2025-07-29 14:14:28 +0100

    Accordion/expansion panel e2e test to TS

---
+++ e2e-tests/T1-accordion-happy-path.spec.ts
@@ -0,0 +1,72 @@
+import { test, expect } from '@playwright/test';
+
+test('Expansion panel visibility', async ({ page }) => {
+  await page.goto('/');
+
+  // Organisation
+  await expect(page.getByRole('heading', { name: 'Organisation' })).toBeVisible();
+  page.pause();
+  await expect(page.getByText('To understand the scale of')).toBeVisible();
+  await expect(
+    page.locator('expansion-panel').filter({ hasText: 'Organisation expand_less To' }).getByLabel('Hide details')
+  ).toBeVisible();
+  await page
+    .locator('expansion-panel')
+    .filter({ hasText: 'Organisation expand_less To' })
+    .getByLabel('Hide details')
+    .click();
+  await expect(page.getByText('To understand the scale of')).not.toBeVisible();
+  await page
+    .locator('expansion-panel')
+    .filter({ hasText: 'Organisation expand_more To' })
+    .getByLabel('Show details')
+    .click();
+  await expect(page.getByText('To understand the scale of')).toBeVisible();
+
+  // On Prem
+  await expect(page.getByRole('heading', { name: 'On-Premise Servers' })).toBeVisible();
+  await expect(
+    page.locator('expansion-panel').filter({ hasText: 'On-Premise Servers' }).getByLabel('Hide details')
+  ).toBeVisible();
+  await page.locator('expansion-panel').filter({ hasText: 'On-Premise Servers' }).getByLabel('Hide details').click();
+  await expect(page.getByText("We'll use the number of")).not.toBeVisible();
+  await page.locator('expansion-panel').filter({ hasText: 'On-Premise Servers' }).getByLabel('Show details').click();
+  await expect(page.getByText("We'll use the number of")).toBeVisible();
+
+  // Cloud
+  await expect(page.getByRole('heading', { name: 'Cloud Services' })).toBeVisible();
+  await expect(
+    page.locator('expansion-panel').filter({ hasText: 'Cloud Services expand_less' }).getByLabel('Hide details')
+  ).toBeVisible();
+  await page
+    .locator('expansion-panel')
+    .filter({ hasText: 'Cloud Services expand_less' })
+    .getByLabel('Hide details')
+    .click();
+  await expect(page.getByText('Cloud Services expand_less')).not.toBeVisible();
+  await page
+    .locator('expansion-panel')
+    .filter({ hasText: 'Cloud Services expand_more' })
+    .getByLabel('Show details')
+    .click();
+  await expect(page.getByText('Cloud Services expand_less')).toBeVisible();
+
+  // Users
+  await expect(page.getByRole('heading', { name: 'End-Users' })).toBeVisible();
+  await expect(
+    page.locator('expansion-panel').filter({ hasText: 'End-Users expand_less Tell us' }).getByLabel('Hide details')
+  ).toBeVisible();
+  await expect(page.getByText('Tell us about your end-users')).toBeVisible();
+  await page
+    .locator('expansion-panel')
+    .filter({ hasText: 'End-Users expand_less Tell us' })
+    .getByLabel('Hide details')
+    .click();
+  await expect(page.getByText('Tell us about your end-users')).not.toBeVisible();
+  await page
+    .locator('expansion-panel')
+    .filter({ hasText: 'End-Users expand_more Tell us' })
+    .getByLabel('Show details')
+    .click();
+  await expect(page.getByText('Tell us about your end-users')).toBeVisible();
+});
