d71d489dd8328a93cd748be1d740861862aa2364
Kieran Ellis <kellis@scottlogic.com>
2025-07-31 10:08:18 +0100

    Update viewport settings

--- playwright.config.ts
+++ playwright.config.ts
@@ -36,7 +36,7 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: { ...devices['Desktop Chrome'], viewport: { width: 1280, height: 720 } },
     },
 
     // {
