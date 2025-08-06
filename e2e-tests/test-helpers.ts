6c1c10432a74a5938b4e0c8d9363c1d36a39563b
Maxwell Nyamunda <142179888+mnyamunda-scottlogic@users.noreply.github.com>
2025-08-06 15:17:06 +0100

    update with new function for cloud

--- e2e-tests/test-helpers.ts
+++ e2e-tests/test-helpers.ts
@@ -24,3 +24,14 @@ export async function onPremiseVisibility(page: Page) {
   await expect(page.getByText('Where are they primarily located? expand_more')).toBeVisible();
   // await expect(page.getByLabel('Where are they primarily')).toHaveValue('WORLD');
 }
+
+export async function cloudVisibility(page: Page) {
+  await expect(page.getByRole('heading', { name: 'Cloud Services' })).toBeVisible();
+  await expect(page.getByText('Cloud 50%')).toBeVisible();
+  await expect(page.getByText('On-premise 50%')).toBeVisible();
+  await expect(page.getByLabel('Where are your cloud servers')).toHaveValue('WORLD');
+  await expect(page.getByLabel('What is your monthly cloud')).toHaveValue('0: Object');
+  await expect(page.getByRole('checkbox', { name: "We don't use cloud services" })).toBeVisible;
+  // await expect(page.getByLabel('What percentage of your')).toBeVisible();
+  // await expect(page.getByRole('slider', { name: 'What percentage of your' })).toHaveValue('50');
+}
