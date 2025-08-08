81ee56f5fba5fbd7d159bc6f5ca052a348eca66d
Maxwell Nyamunda <142179888+mnyamunda-scottlogic@users.noreply.github.com>
2025-08-08 10:54:41 +0100

    change locations,  of accessibility helpers, update + optimise

--- e2e-tests/test-helpers.ts
+++ e2e-tests/test-helpers.ts
@@ -1,4 +1,10 @@
+import AxeBuilder from '@axe-core/playwright';
 import { Page, expect } from '@playwright/test';
+
+export const expectNoA11yViolations = async (page: Page) => {
+  const results = await new AxeBuilder({ page }).analyze();
+  expect(results.violations).toEqual([]);
+};
 
 export async function gotoHome(page: Page) {
   await page.goto('/');
