59cbd708bcea2b4fd96027ec724273c3a52662ea
Maxwell Nyamunda <142179888+mnyamunda-scottlogic@users.noreply.github.com>
2025-08-12 12:12:43 +0100

    optimise t18 with beforeEach

--- e2e-tests/T18-table-accordion-calculations.spec.ts
+++ e2e-tests/T18-table-accordion-calculations.spec.ts
@@ -2,11 +2,13 @@ import { test, expect } from '@playwright/test';
 import { gotoHome, assertAllSectionElementsAreVisible, assertDefaultTableStructure } from './test-helpers';
 
 test.describe('Table Accordion Calculations', async () => {
-  test('Table shows expected values (no checkboxes)', async ({ page }) => {
+  test.beforeEach(async ({ page }) => {
     await gotoHome(page);
     await assertAllSectionElementsAreVisible(page);
     await page.getByRole('tab', { name: 'Table' }).click();
     await assertDefaultTableStructure(page);
+  });
+  test('Table shows expected values (no checkboxes)', async ({ page }) => {
     await page.getByRole('button', { name: 'Calculate' }).click();
 
     const expectedEmissions = ['34%', '25%', '7%', '2%', '65%', '12%', '47%', '6%', '1%', '1%', '<1%', '<1%', '<1%'];
@@ -14,14 +16,11 @@ test.describe('Table Accordion Calculations', async () => {
     await expect(emissionCells).toHaveText(expectedEmissions);
   });
   test('Table shows expected values (On-Premise is unknown)', async ({ page }) => {
-    await gotoHome(page);
-    await assertAllSectionElementsAreVisible(page);
     await page.getByRole('tab', { name: 'Table' }).click();
     await assertDefaultTableStructure(page);
     await page.getByLabel("I don't know").check();
     await page.getByRole('button', { name: 'Calculate' }).click();
 
-    //assert default array
     const expectedEmissionsOnPremiseUnknownArray = [
       '42%',
       '34%',
@@ -41,15 +40,10 @@ test.describe('Table Accordion Calculations', async () => {
     await expect(emissionCells).toHaveText(expectedEmissionsOnPremiseUnknownArray);
   });
   test('Table shows expected values (Cloud services not used)', async ({ page }) => {
-    await gotoHome(page);
-    await assertAllSectionElementsAreVisible(page);
-    await page.getByRole('tab', { name: 'Table' }).click();
-    await assertDefaultTableStructure(page);
     await page.getByLabel("We don't use cloud services").check();
     await page.getByRole('button', { name: 'Calculate' }).click();
     await expect(page.getByRole('gridcell', { name: 'Cloud Services', exact: true })).not.toBeVisible();
 
-    //assert default array
     const expectedEmissionsCloudNotUsedArray = [
       '34%',
       '25%',
@@ -68,10 +62,6 @@ test.describe('Table Accordion Calculations', async () => {
     await expect(emissionCells).toHaveText(expectedEmissionsCloudNotUsedArray);
   });
   test('Table shows expected values (No external users)', async ({ page }) => {
-    await gotoHome(page);
-    await assertAllSectionElementsAreVisible(page);
-    await page.getByRole('tab', { name: 'Table' }).click();
-    await assertDefaultTableStructure(page);
     await page.getByRole('checkbox', { name: "We don't have any external" }).check();
     await page.getByRole('button', { name: 'Calculate' }).click();
     await expect(page.getByRole('gridcell', { name: 'End-User Devices', exact: true })).not.toBeVisible();
@@ -86,9 +76,8 @@ test.describe('Table Accordion Calculations', async () => {
       '12%',
       '48%',
       '6%',
-      '<1%',
-      '<1%',
-      '<1%',
+      '1%',
+      '1%',
       '<1%',
     ];
     const emissionCells = page.locator('td:nth-child(2)');
