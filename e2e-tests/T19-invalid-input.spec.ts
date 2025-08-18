8c5a4135e1d19c6d86fa07fae39b6fed972f8bf0
Maxwell Nyamunda <142179888+mnyamunda-scottlogic@users.noreply.github.com>
2025-08-18 12:25:56 +0100

    update with different cases

--- e2e-tests/T19-invalid-input.spec.ts
+++ e2e-tests/T19-invalid-input.spec.ts
@@ -12,22 +12,70 @@ test.describe('Default page load and error handling', () => {
     await assertAllSectionElementsAreVisible(page);
     await resultsTabVisibilityCheck(page);
   });
-  test('Number of servers must be >= 0', async ({ page }) => {
+  test('Assert error when number of servers is -1', async ({ page }) => {
     await spinButtonFill(page, 'Number of Servers:', '-1');
+    await expect(page.locator('#numberOfServersError').getByText('The number of servers')).toBeVisible();
     await page.getByRole('button', { name: 'Calculate' }).click();
     await expect(page.getByRole('link', { name: 'The number of servers must be' })).toBeVisible();
+  });
 
+  test('Assert no Error when number of servers is 0', async ({ page }) => {
     await spinButtonFill(page, 'Number of Servers:', '0');
+    await expect(page.locator('#numberOfServersError').getByText('The number of servers')).not.toBeVisible();
     await page.getByRole('button', { name: 'Calculate' }).click();
     await expect(page.getByRole('link', { name: 'The number of servers must be' })).not.toBeVisible();
-
-    // await expect(page.locator('foreignobject')).toHaveScreenshot('T0-apex-chart.png');
   });
-  test('Number of employees must be > 0 ', async ({ page }) => {
+
+  test('Assert error when number of employees is 0 ', async ({ page }) => {
+    //initial msg
     await spinButtonFill(page, 'How many employees are in the', '0');
+    await expect(page.locator('#headCountError').getByText('The number of employees')).toBeVisible();
+    //message after clicking calculate
     await page.getByRole('button', { name: 'Calculate' }).click();
     await expect(page.getByRole('link', { name: 'The number of employees must be' })).toBeVisible();
+  });
+  test('Assert error when number of employees is -1 ', async ({ page }) => {
+    //initial msg
+    await spinButtonFill(page, 'How many employees are in the', '-1');
+    await expect(page.locator('#headCountError').getByText('The number of employees')).toBeVisible();
+    //message after clicking calculate
+    await page.getByRole('button', { name: 'Calculate' }).click();
+    await expect(page.getByRole('link', { name: 'The number of employees must be' })).toBeVisible();
+  });
 
-    // await expect(page.locator('foreignobject')).toHaveScreenshot('T0-apex-chart.png');
+  test('Assert no error when number of employees is 1 ', async ({ page }) => {
+    //initial msg
+    await spinButtonFill(page, 'How many employees are in the', '1');
+    await expect(page.locator('#headCountError').getByText('The number of employees')).not.toBeVisible();
+    //message after clicking calculate
+    await page.getByRole('button', { name: 'Calculate' }).click();
+    await expect(page.getByRole('link', { name: 'The number of employees must be' })).not.toBeVisible();
+  });
+
+  test('Asser error when number of users is 0 ', async ({ page }) => {
+    //initial msg
+    await spinButtonFill(page, 'How many monthly active users', '0');
+    await expect(page.locator('#monthlyActiveUsersError').getByText('The number of monthly active')).toBeVisible();
+    //message after clicking calculate
+    await page.getByRole('button', { name: 'Calculate' }).click();
+    await expect(page.getByRole('link', { name: 'The number of monthly active' })).toBeVisible();
+  });
+
+  test('Assert error when number of users is -1 ', async ({ page }) => {
+    //initial msg
+    await spinButtonFill(page, 'How many monthly active users', '-1');
+    await expect(page.locator('#monthlyActiveUsersError').getByText('The number of monthly active')).toBeVisible();
+    //message after clicking calculate
+    await page.getByRole('button', { name: 'Calculate' }).click();
+    await expect(page.getByRole('link', { name: 'The number of monthly active' })).toBeVisible();
+  });
+
+  test('Assert no error when number of users is 1 ', async ({ page }) => {
+    //initial msg
+    await spinButtonFill(page, 'How many monthly active users', '1');
+    await expect(page.locator('#monthlyActiveUsersError').getByText('The number of monthly active')).not.toBeVisible();
+    //message after clicking calculate
+    await page.getByRole('button', { name: 'Calculate' }).click();
+    await expect(page.getByRole('link', { name: 'The number of monthly active' })).not.toBeVisible();
   });
 });
