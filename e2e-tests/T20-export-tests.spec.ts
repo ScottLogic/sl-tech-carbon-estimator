7ff98c7ca2280c44dc8db071c46514c78a8c27de
Maxwell Nyamunda <142179888+mnyamunda-scottlogic@users.noreply.github.com>
2025-09-11 11:05:54 +0100

    Whole json assertion test

--- e2e-tests/T20-export-tests.spec.ts
+++ e2e-tests/T20-export-tests.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from './fixtures';
 import { assertAllSectionElementsAreVisible } from './test-helpers';
+import { createDefaultCalculationJsonExport } from './test-helpers';
 import * as fs from 'fs';
 
 test('T20', async ({
@@ -38,57 +39,8 @@ test('T20', async ({
   const fileContent = fs.readFileSync(path, 'utf-8');
   const json = JSON.parse(fileContent);
 
-  // expect(json.values.version).toEqual('1.0.0');
-  expect(json).toEqual({
-    values: {
-      version: '0.0.0-semantically-released',
-      upstreamEmissions: {
-        employee: 13708.333333333334,
-        server: 3625,
-        network: 1300,
-        software: 0,
-      },
-      directEmissions: {
-        employee: 6484.5694,
-        server: 26189.56176,
-        network: 3093.2690302799997,
-      },
-      indirectEmissions: {
-        cloud: 620.874264,
-        saas: 0,
-        managed: 0,
-      },
-      downstreamEmissions: {
-        endUser: 147.60952776716255,
-        networkTransfer: 239.029161520401,
-        downstreamInfrastructure: 0,
-      },
-      totalEmissions: 55408.2464769009,
-    },
-    percentages: {
-      version: '0.0.0-semantically-released',
-      upstreamEmissions: {
-        employee: 24.740601273220566,
-        server: 6.542347449149511,
-        network: 2.346221154177756,
-        software: 0,
-      },
-      directEmissions: {
-        employee: 11.70325684770289,
-        server: 47.26654139996678,
-        network: 5.582687103389114,
-      },
-      indirectEmissions: {
-        cloud: 1.1205448709856498,
-        saas: 0,
-        managed: 0,
-      },
-      downstreamEmissions: {
-        endUser: 0.2664035358503889,
-        networkTransfer: 0.4313963655573357,
-        downstreamInfrastructure: 0,
-      },
-      totalEmissions: 55408.2464769009,
-    },
+  const expectedJsonContent = createDefaultCalculationJsonExport({
+    // values: { version: '0.0.0-semantically-released' },
   });
+  expect(json).toEqual(expectedJsonContent);
 });
--- e2e-tests/test-helpers.ts
+++ e2e-tests/test-helpers.ts
@@ -18,59 +18,114 @@ export async function assertAllSectionElementsAreVisible(
   await endUsersSection.assertEndUserSectionVisible();
 }
 
-export interface EmissionsValues {
+interface EmissionsValues {
   version: string;
-  upstreamEmissions: {
+  upstreamEmissions?: {
     employee: number;
     server: number;
     network: number;
     software: number;
   };
-  directEmissions: {
+  directEmissions?: {
     employee: number;
     server: number;
     network: number;
   };
-  indirectEmissions: {
+  indirectEmissions?: {
     cloud: number;
     saas: number;
     managed: number;
   };
-  downstreamEmissions: {
+  downstreamEmissions?: {
     endUser: number;
     networkTransfer: number;
     downstreamInfrastructure: number;
   };
-  totalEmissions: number;
+  totalEmissions?: number;
 }
 
-export interface EmissionsPercentages {
+interface EmissionsPercentages {
   version: string;
-  upstreamEmissions: {
+  upstreamEmissions?: {
     employee: number;
     server: number;
     network: number;
     software: number;
   };
-  directEmissions: {
+  directEmissions?: {
     employee: number;
     server: number;
     network: number;
   };
-  indirectEmissions: {
+  indirectEmissions?: {
     cloud: number;
     saas: number;
     managed: number;
   };
-  downstreamEmissions: {
+  downstreamEmissions?: {
     endUser: number;
     networkTransfer: number;
     downstreamInfrastructure: number;
   };
-  totalEmissions: number;
+  totalEmissions?: number;
 }
 
 export interface EmissionsData {
   values: EmissionsValues;
   percentages: EmissionsPercentages;
+}
+export function createDefaultCalculationJsonExport(overrides: Partial<EmissionsData> = {}): EmissionsData {
+  const defaultCalculationJson = {
+    values: {
+      version: '0.0.0-semantically-released',
+      upstreamEmissions: {
+        employee: 13708.333333333334,
+        server: 3625,
+        network: 1300,
+        software: 0,
+      },
+      directEmissions: {
+        employee: 6484.5694,
+        server: 26189.56176,
+        network: 3093.2690302799997,
+      },
+      indirectEmissions: {
+        cloud: 620.874264,
+        saas: 0,
+        managed: 0,
+      },
+      downstreamEmissions: {
+        endUser: 147.60952776716255,
+        networkTransfer: 239.029161520401,
+        downstreamInfrastructure: 0,
+      },
+      totalEmissions: 55408.2464769009,
+    },
+    percentages: {
+      version: '0.0.0-semantically-released',
+      upstreamEmissions: {
+        employee: 24.740601273220566,
+        server: 6.542347449149511,
+        network: 2.346221154177756,
+        software: 0,
+      },
+      directEmissions: {
+        employee: 11.70325684770289,
+        server: 47.26654139996678,
+        network: 5.582687103389114,
+      },
+      indirectEmissions: {
+        cloud: 1.1205448709856498,
+        saas: 0,
+        managed: 0,
+      },
+      downstreamEmissions: {
+        endUser: 0.2664035358503889,
+        networkTransfer: 0.4313963655573357,
+        downstreamInfrastructure: 0,
+      },
+      totalEmissions: 55408.2464769009,
+    },
+  };
+  return { ...defaultCalculationJson, ...overrides };
 }
