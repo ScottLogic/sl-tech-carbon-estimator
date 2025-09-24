ff1a8970e3b11bf9051141f8dd85978b420ec812
Daniel Moorhouse <dmoorhouse@scottlogic.com>
2025-09-24 13:56:43 +0100

    Update vite config, add build and run commands in package.json

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,5 @@
-export const plugins = {
-  tailwindcss: {},
+export default { 
+  plugins: {
+    tailwindcss: {},
+  },
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,45 @@
 import { defineConfig } from 'vite';
 import angular from '@analogjs/vite-plugin-angular';
 import string from 'vite-plugin-string';
+import {resolve} from 'path';
 
 export default defineConfig({
   plugins: [
     angular(),
     string({
-      include: ['**/*.generated.css', '*.css'] // Import .css files as strings
+      include: ['**/*.generated.css', '**/*.css'] // Import .css files as strings
     }),
   ],
+  root: 'src',
   build: {
-    lib: {
-      entry: 'src/main.ts',
-      name: 'TechCarbonEstimator',
-      fileName: 'main',
-      formats: ['es', 'umd'],
-    },
+    outDir: '../dist/tech-carbon-estimator',
+    emptyOutDir: true,
     rollupOptions: {
-      // Externalize Angular dependencies for npm package
+      input: {
+        main: resolve(__dirname, 'src/package-index.html'),
+      },
       external: [
         '@angular/core',
         '@angular/common',
         '@angular/elements',
-        // ...add others as needed
+        'zone.js',
       ],
       output: {
         globals: {
           '@angular/core': 'ng.core',
           '@angular/common': 'ng.common',
           '@angular/elements': 'ng.elements',
+          'zone.js': 'Zone',
         },
       },
     },
+    lib: {
+      entry: resolve(__dirname, 'src/main.ts'),
+      name: 'TechCarbonEstimator',
+      fileName: 'main',
+      formats: ['es', 'umd'],
+    },    
   },
+  publicDir: false,
+  assetsInclude: ['**/*.ico', '**/*.css'],
 });
