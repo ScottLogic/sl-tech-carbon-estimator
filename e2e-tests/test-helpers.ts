ae795dd8b4b5c289cced50a67077216815af7134
Maxwell Nyamunda <142179888+mnyamunda-scottlogic@users.noreply.github.com>
2025-10-10 15:53:36 +0100

    update helpers endusers -> customers

--- e2e-tests/test-helpers.ts
+++ e2e-tests/test-helpers.ts
@@ -3,19 +3,19 @@ import type { Page } from '@playwright/test';
 import { expect } from '@playwright/test';
 import { OrganisationSection } from './page-objects/organisation-section';
 import { CloudServicesSection } from './page-objects/cloud-services-section';
-import { EndUsersSection } from './page-objects/customers-section';
+import { CustomersSection } from './page-objects/customers-section';
 import { OnPremSection } from './page-objects/on-prem-section';
 
 export async function assertAllSectionElementsAreVisible(
   organisationSection: OrganisationSection,
   onPremSection: OnPremSection,
   cloudServicesSection: CloudServicesSection,
-  endUsersSection: EndUsersSection
+  customersSection: CustomersSection
 ) {
   await organisationSection.assertOrganisationSectionVisible();
   await onPremSection.assertOnPremiseSectionVisible();
   await cloudServicesSection.assertDefaultCloudElementVisibility();
-  await endUsersSection.assertEndUserSectionVisible();
+  await customersSection.assertEndUserSectionVisible();
 }
 
 export const expectNoA11yViolations = async (page: Page) => {
