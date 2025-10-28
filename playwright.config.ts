96dddd7b0804bf446d3bc6cc343e692562f2d1ab
Maxwell Nyamunda <142179888+mnyamunda-scottlogic@users.noreply.github.com>
2025-10-28 10:31:16 +0000

    update snapshot directory

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,6 +13,7 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './e2e-tests',
+  snapshotDir: './e2e-tests/snapshot-directory',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
