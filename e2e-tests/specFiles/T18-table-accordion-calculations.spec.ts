e2c81f4645ea317ae374ff803c06c247f5e387c3
Maxwell Nyamunda <142179888+mnyamunda-scottlogic@users.noreply.github.com>
2025-10-28 10:44:15 +0000

    uncomment tests

--- e2e-tests/specFiles/T18-table-accordion-calculations.spec.ts
+++ e2e-tests/specFiles/T18-table-accordion-calculations.spec.ts
@@ -1,93 +1,92 @@
-// import * as TestData from '../test-data';
-// import { test, expect } from './fixtures';
-// import { assertAllSectionElementsAreVisible } from '../test-helpers';
-// import { table } from 'console';
+import * as TestData from '../test-data';
+import { test, expect } from '../utilities/fixtures';
+import { assertAllSectionElementsAreVisible } from '../test-helpers';
 
-// test.describe('Table Accordion Calculations', async () => {
-//   test.beforeEach(
-//     async ({
-//       tcsEstimator,
-//       organisationSection,
-//       onPremSection,
-//       cloudServicesSection,
-//       customersSection,
-//       estimationsSection,
-//       tableSection,
-//     }) => {
-//       await tcsEstimator.gotoHome();
-//       await assertAllSectionElementsAreVisible(
-//         organisationSection,
-//         onPremSection,
-//         cloudServicesSection,
-//         customersSection
-//       );
+test.describe('Table Accordion Calculations', async () => {
+  test.beforeEach(
+    async ({
+      tcsEstimator,
+      organisationSection,
+      onPremSection,
+      cloudServicesSection,
+      customersSection,
+      estimationsSection,
+      tableSection,
+    }) => {
+      await tcsEstimator.gotoHome();
+      await assertAllSectionElementsAreVisible(
+        organisationSection,
+        onPremSection,
+        cloudServicesSection,
+        customersSection
+      );
 
-//       await estimationsSection.tableViewButton.click();
-//       await tableSection.assertDefaultTableStructure;
-//     }
-//   );
-//   test('Table shows expected values (no checkboxes)', async ({ estimationsSection, tcsEstimator, tableSection }) => {
-//     await tcsEstimator.calculateButton.click();
+      await estimationsSection.tableViewButton.click();
+      await tableSection.assertDefaultTableStructure;
+    }
+  );
+  test('Table shows expected values (no checkboxes)', async ({ estimationsSection, tcsEstimator, tableSection }) => {
+    await tcsEstimator.calculateButton.click();
 
-//     await tableSection.assertCorrectKilogramColumnValues(TestData.t18NoCheckBoxesExpectedEmissionKilogramsAnnual);
-//     await estimationsSection.monthlyViewButton.click();
-//     await tableSection.assertCorrectKilogramColumnValues(TestData.t18NoCheckBoxesExpectedEmissionKilogramsMonthly);
-//     await tableSection.assertCorrectPercentageColumnValues(TestData.t18NoCheckBoxesExpectedEmissionPercentages);
-//   });
-//   test('Table shows expected values (On-Premise is unknown)', async ({
-//     tcsEstimator,
-//     onPremSection,
-//     estimationsSection,
-//     tableSection,
-//   }) => {
-//     await estimationsSection.tableViewButton.click();
-//     await onPremSection.onPremUnknownTickbox.check();
-//     await tcsEstimator.calculateButton.click();
+    await tableSection.assertCorrectKilogramColumnValues(TestData.t18NoCheckBoxesExpectedEmissionKilogramsAnnual);
+    await estimationsSection.monthlyViewButton.click();
+    await tableSection.assertCorrectKilogramColumnValues(TestData.t18NoCheckBoxesExpectedEmissionKilogramsMonthly);
+    await tableSection.assertCorrectPercentageColumnValues(TestData.t18NoCheckBoxesExpectedEmissionPercentages);
+  });
+  test('Table shows expected values (On-Premise is unknown)', async ({
+    tcsEstimator,
+    onPremSection,
+    estimationsSection,
+    tableSection,
+  }) => {
+    await estimationsSection.tableViewButton.click();
+    await onPremSection.onPremUnknownTickbox.check();
+    await tcsEstimator.calculateButton.click();
 
-//     await tableSection.assertCorrectKilogramColumnValues(
-//       TestData.t18ExpectedEmissionKilogramsAnnualOnPremiseUnknownArray
-//     );
-//     await estimationsSection.monthlyViewButton.click();
-//     await tableSection.assertCorrectKilogramColumnValues(
-//       TestData.t18ExpectedEmissionKilogramsMonthlyOnPremiseUnknownArray
-//     );
-//     await tableSection.assertCorrectPercentageColumnValues(
-//       TestData.t18ExpectedEmissionsOnPremiseUnknownPercentagesArray
-//     );
-//   });
-//   test('Table shows expected values (Cloud services not used)', async ({
-//     tcsEstimator,
-//     cloudServicesSection,
-//     tableSection,
-//     estimationsSection,
-//   }) => {
-//     await cloudServicesSection.cloudUnusedTickbox.check();
-//     await tcsEstimator.calculateButton.click();
-//     await expect(tableSection.cloudServices).not.toBeVisible();
+    await tableSection.assertCorrectKilogramColumnValues(
+      TestData.t18ExpectedEmissionKilogramsAnnualOnPremiseUnknownArray
+    );
+    await estimationsSection.monthlyViewButton.click();
+    await tableSection.assertCorrectKilogramColumnValues(
+      TestData.t18ExpectedEmissionKilogramsMonthlyOnPremiseUnknownArray
+    );
+    await tableSection.assertCorrectPercentageColumnValues(
+      TestData.t18ExpectedEmissionsOnPremiseUnknownPercentagesArray
+    );
+  });
+  test('Table shows expected values (Cloud services not used)', async ({
+    tcsEstimator,
+    cloudServicesSection,
+    tableSection,
+    estimationsSection,
+  }) => {
+    await cloudServicesSection.cloudUnusedTickbox.check();
+    await tcsEstimator.calculateButton.click();
+    await expect(tableSection.cloudServices).not.toBeVisible();
 
-//     await tableSection.assertCorrectKilogramColumnValues(TestData.t18ExpectedAnnualEmissionKilogramsCloudNotUsedArray);
-//     await estimationsSection.monthlyViewButton.click();
-//     await tableSection.assertCorrectKilogramColumnValues(TestData.t18ExpectedMonthlyEmissionKilogramsCloudNotUsedArray);
-//     await tableSection.assertCorrectPercentageColumnValues(TestData.t18ExpectedEmissionsCloudNotUsedPercentagesArray);
-//   });
-//   test('Table shows expected values (No external users)', async ({
-//     estimationsSection,
-//     tcsEstimator,
-//     customersSection,
-//     tableSection,
-//   }) => {
-//     await customersSection.customersUnusedTickbox.check();
-//     await tcsEstimator.calculateButton.click();
-//     await expect(tableSection.customerDevices).not.toBeVisible();
-//     await expect(tableSection.networkDataTransfer).not.toBeVisible();
+    await tableSection.assertCorrectKilogramColumnValues(TestData.t18ExpectedAnnualEmissionKilogramsCloudNotUsedArray);
+    await estimationsSection.monthlyViewButton.click();
+    await tableSection.assertCorrectKilogramColumnValues(TestData.t18ExpectedMonthlyEmissionKilogramsCloudNotUsedArray);
+    await tableSection.assertCorrectPercentageColumnValues(TestData.t18ExpectedEmissionsCloudNotUsedPercentagesArray);
+  });
+  test('Table shows expected values (No external users)', async ({
+    estimationsSection,
+    tcsEstimator,
+    customersSection,
+    tableSection,
+  }) => {
+    await customersSection.customersUnusedTickbox.check();
+    await tcsEstimator.calculateButton.click();
+    await expect(tableSection.customerDevices).not.toBeVisible();
+    await expect(tableSection.networkDataTransfer).not.toBeVisible();
 
-//     await tableSection.assertCorrectKilogramColumnValues(
-//       TestData.t18ExpectedAnnualEmissionKilogramsNoExternalUsersArray
-//     );
-//     await estimationsSection.monthlyViewButton.click();
-//     await tableSection.assertCorrectKilogramColumnValues(
-//       TestData.t18ExpectedMonthlyEmissionKilogramsNoExternalUsersArray
-//     );
-//     await tableSection.assertCorrectPercentageColumnValues(TestData.t18ExpectedEmissionPercentagesNoExternalUsersArray);
-//   });
-// });
+    await tableSection.assertCorrectKilogramColumnValues(
+      TestData.t18ExpectedAnnualEmissionKilogramsNoExternalUsersArray
+    );
+    await estimationsSection.monthlyViewButton.click();
+    await tableSection.assertCorrectKilogramColumnValues(
+      TestData.t18ExpectedMonthlyEmissionKilogramsNoExternalUsersArray
+    );
+    await tableSection.assertCorrectPercentageColumnValues(TestData.t18ExpectedEmissionPercentagesNoExternalUsersArray);
+  });
+});
--- e2e-tests/specFiles/T20-export-tests.spec.ts
+++ e2e-tests/specFiles/T20-export-tests.spec.ts
@@ -1,82 +1,82 @@
-// import { test, expect } from './fixtures';
-// import {
-//   createDefaultPercentagesJsonExport,
-//   createDefaultInputJsonExport,
-//   createDefaultValuesJsonExport,
-// } from '../test-helpers';
-// import * as TestData from '../test-data';
+import { test, expect } from '../utilities/fixtures';
+import {
+  createDefaultPercentagesJsonExport,
+  createDefaultInputJsonExport,
+  createDefaultValuesJsonExport,
+} from '../test-helpers';
+import * as TestData from '../test-data';
 
-// test.describe('Export JSON files', () => {
-//   test.beforeEach(
-//     async ({
-//       organisationSection,
-//       tcsEstimator,
-//       onPremSection,
-//       cloudServicesSection,
-//       customersSection,
-//       estimationsSection,
-//       tableSection,
-//     }) => {
-//       await tcsEstimator.gotoHome();
-//       await organisationSection.assertOrganisationSectionVisible();
-//       await onPremSection.assertOnPremiseSectionVisible();
-//       await cloudServicesSection.assertDefaultCloudElementVisibility();
-//       await customersSection.assertCustomersSectionVisible();
+test.describe('Export JSON files', () => {
+  test.beforeEach(
+    async ({
+      organisationSection,
+      tcsEstimator,
+      onPremSection,
+      cloudServicesSection,
+      customersSection,
+      estimationsSection,
+      tableSection,
+    }) => {
+      await tcsEstimator.gotoHome();
+      await organisationSection.assertOrganisationSectionVisible();
+      await onPremSection.assertOnPremiseSectionVisible();
+      await cloudServicesSection.assertDefaultCloudElementVisibility();
+      await customersSection.assertCustomersSectionVisible();
 
-//       await estimationsSection.assertResultsElementVisibility();
-//       await estimationsSection.tableViewButton.click();
-//       await tableSection.assertDefaultTableStructure();
-//       await estimationsSection.diagramViewButton.click();
-//       await expect(estimationsSection.exportButton).toBeDisabled();
-//       await tcsEstimator.calculateButton.click();
-//     }
-//   );
+      await estimationsSection.assertResultsElementVisibility();
+      await estimationsSection.tableViewButton.click();
+      await tableSection.assertDefaultTableStructure();
+      await estimationsSection.diagramViewButton.click();
+      await expect(estimationsSection.exportButton).toBeDisabled();
+      await tcsEstimator.calculateButton.click();
+    }
+  );
 
-//   test('T20 Export and read JSON (Annual)', async ({ page, estimationsSection }) => {
-//     const downloadPath = await estimationsSection.exportJsonContent(page, 'Export JSON');
-//     const jsonParse = await estimationsSection.readJsonFileContent(downloadPath);
+  test('T20 Export and read JSON (Annual)', async ({ page, estimationsSection }) => {
+    const downloadPath = await estimationsSection.exportJsonContent(page, 'Export JSON');
+    const jsonParse = await estimationsSection.readJsonFileContent(downloadPath);
 
-//     const expectedAnnualValuesJsonContent = createDefaultValuesJsonExport({});
-//     const expectedPercentagesJsonContent = createDefaultPercentagesJsonExport({});
+    const expectedAnnualValuesJsonContent = createDefaultValuesJsonExport({});
+    const expectedPercentagesJsonContent = createDefaultPercentagesJsonExport({});
 
-//     expect(jsonParse.values).toEqual(expectedAnnualValuesJsonContent.values);
-//     expect(jsonParse.percentages).toEqual(expectedPercentagesJsonContent.percentages);
-//   });
+    expect(jsonParse.values).toEqual(expectedAnnualValuesJsonContent.values);
+    expect(jsonParse.percentages).toEqual(expectedPercentagesJsonContent.percentages);
+  });
 
-//   test('T20 Export and read JSON (Monthly)', async ({ page, estimationsSection }) => {
-//     await estimationsSection.monthlyViewButton.click();
-//     const downloadPath = await estimationsSection.exportJsonContent(page, 'Export JSON');
-//     const jsonParse = await estimationsSection.readJsonFileContent(downloadPath);
+  test('T20 Export and read JSON (Monthly)', async ({ page, estimationsSection }) => {
+    await estimationsSection.monthlyViewButton.click();
+    const downloadPath = await estimationsSection.exportJsonContent(page, 'Export JSON');
+    const jsonParse = await estimationsSection.readJsonFileContent(downloadPath);
 
-//     const expectedPercentagesJsonContent = createDefaultPercentagesJsonExport({});
+    const expectedPercentagesJsonContent = createDefaultPercentagesJsonExport({});
 
-//     expect(jsonParse.values).toEqual(TestData.t20ExpectedMonthlyValuesJson.values);
-//     expect(jsonParse.percentages).toEqual(expectedPercentagesJsonContent.percentages);
-//   });
+    expect(jsonParse.values).toEqual(TestData.t20ExpectedMonthlyValuesJson.values);
+    expect(jsonParse.percentages).toEqual(expectedPercentagesJsonContent.percentages);
+  });
 
-//   test('T20 Export and read JSON with inputs (Annual)', async ({ page, estimationsSection }) => {
-//     const downloadPath = await estimationsSection.exportJsonContent(page, 'Export JSON with Inputs');
-//     const jsonParse = await estimationsSection.readJsonFileContent(downloadPath);
+  test('T20 Export and read JSON with inputs (Annual)', async ({ page, estimationsSection }) => {
+    const downloadPath = await estimationsSection.exportJsonContent(page, 'Export JSON with Inputs');
+    const jsonParse = await estimationsSection.readJsonFileContent(downloadPath);
 
-//     const expectedValuesJsonContent = createDefaultValuesJsonExport({});
-//     const expectedPercentagesJsonContent = createDefaultPercentagesJsonExport({});
-//     const expectedInputsJsonContent = createDefaultInputJsonExport({});
+    const expectedValuesJsonContent = createDefaultValuesJsonExport({});
+    const expectedPercentagesJsonContent = createDefaultPercentagesJsonExport({});
+    const expectedInputsJsonContent = createDefaultInputJsonExport({});
 
-//     expect(jsonParse.estimate.values).toEqual(expectedValuesJsonContent.values);
-//     expect(jsonParse.estimate.percentages).toEqual(expectedPercentagesJsonContent.percentages);
-//     expect(jsonParse.input).toEqual(expectedInputsJsonContent.input);
-//   });
+    expect(jsonParse.estimate.values).toEqual(expectedValuesJsonContent.values);
+    expect(jsonParse.estimate.percentages).toEqual(expectedPercentagesJsonContent.percentages);
+    expect(jsonParse.input).toEqual(expectedInputsJsonContent.input);
+  });
 
-//   test('T20 Export and read JSON with inputs (Monthly)', async ({ page, estimationsSection }) => {
-//     await estimationsSection.monthlyViewButton.click();
-//     const downloadPath = await estimationsSection.exportJsonContent(page, 'Export JSON with Inputs');
-//     const jsonParse = await estimationsSection.readJsonFileContent(downloadPath);
+  test('T20 Export and read JSON with inputs (Monthly)', async ({ page, estimationsSection }) => {
+    await estimationsSection.monthlyViewButton.click();
+    const downloadPath = await estimationsSection.exportJsonContent(page, 'Export JSON with Inputs');
+    const jsonParse = await estimationsSection.readJsonFileContent(downloadPath);
 
-//     const expectedPercentagesJsonContent = createDefaultPercentagesJsonExport({});
-//     const expectedInputsJsonContent = createDefaultInputJsonExport({});
+    const expectedPercentagesJsonContent = createDefaultPercentagesJsonExport({});
+    const expectedInputsJsonContent = createDefaultInputJsonExport({});
 
-//     expect(jsonParse.estimate.values).toEqual(TestData.t20ExpectedMonthlyValuesJson.values);
-//     expect(jsonParse.estimate.percentages).toEqual(expectedPercentagesJsonContent.percentages);
-//     expect(jsonParse.input).toEqual(expectedInputsJsonContent.input);
-//   });
-// });
+    expect(jsonParse.estimate.values).toEqual(TestData.t20ExpectedMonthlyValuesJson.values);
+    expect(jsonParse.estimate.percentages).toEqual(expectedPercentagesJsonContent.percentages);
+    expect(jsonParse.input).toEqual(expectedInputsJsonContent.input);
+  });
+});
