ddbb6c65e86bafb3fab92f428258f18620b802a0
Maxwell Nyamunda <142179888+mnyamunda-scottlogic@users.noreply.github.com>
2025-10-29 12:21:40 +0000

    refactor(automation): rename and move files

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,8 +12,8 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './e2e-tests',
-  snapshotDir: './e2e-tests/snapshot-directory',
+  testDir: './playwright-tests',
+  snapshotDir: './playwright-tests/snapshot-directory',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
