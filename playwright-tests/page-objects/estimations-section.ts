828c0b55d0c2ef9586b2dbb38bff20ff425c46cb
Maxwell Nyamunda <142179888+mnyamunda-scottlogic@users.noreply.github.com>
2025-11-13 16:29:29 +0000

    test(automation): pOM changes

--- playwright-tests/page-objects/estimations-section.ts
+++ playwright-tests/page-objects/estimations-section.ts
@@ -6,6 +6,10 @@ export class EstimationsSection {
   public readonly diagramViewButton: Locator;
   public readonly tableViewButton: Locator;
   public readonly exportButton: Locator;
+  public readonly exportJsonButton: Locator;
+  public readonly exportJsonInputsButton: Locator;
+  public readonly exportPdfButton: Locator;
+  public readonly downloadPdfButton: Locator;
   public readonly monthlyViewButton: Locator;
   public readonly annualViewButton: Locator;
 
@@ -15,6 +19,10 @@ export class EstimationsSection {
     this.exportButton = page.getByRole('button', { name: 'Export ▼' });
     this.monthlyViewButton = page.getByText('Monthly', { exact: true });
     this.annualViewButton = page.getByText('Annual', { exact: true });
+    this.downloadPdfButton = page.getByRole('button', { name: 'Download PDF' });
+    this.exportJsonButton = page.getByRole('link', { name: 'Export JSON', exact: true });
+    this.exportJsonInputsButton = page.getByRole('link', { name: 'Export JSON with Inputs', exact: true });
+    this.exportPdfButton = page.getByRole('button', { name: 'Export PDF' });
   }
   async assertResultsElementVisibility() {
     await expect(this.diagramViewButton).toBeVisible();
@@ -22,7 +30,7 @@ export class EstimationsSection {
     await expect(this.exportButton).toBeVisible();
   }
 
-  async exportJsonContent(page: Page, exportType: 'Export JSON' | 'Export JSON with Inputs') {
+  async exportJsonContent(page: Page, exportType: 'Export JSON' | 'Export JSON with Inputs' | 'Export PDF') {
     const [download] = await Promise.all([
       page.waitForEvent('download'),
       this.exportButton.click(),
