d7fd12dad24efd6ede664159cb4fc038005a23ba
Dan Malecki <dmalecki@scottlogic.com>
2025-11-21 10:01:51 +0000

    refactor(lint/config): consolidate html rules & drop accessibility plugin
    * refactor(lint/config): consolidate html rules & remove accessibility plugin
    
    - Removed the unnecessary inline-template block (no inline-template files in proj)
    - Moved duplicate/split html blocks into one unified block
    - Moved 'endOfLine' options from eslint.config.mjs to .prettierrc.json for central control
    - Applied prettier formatting to eslint.config.mjs
    
    * chore: enforce LF line endings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,55 +1,42 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import { defineConfig, globalIgnores } from 'eslint/config';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-export default defineConfig([globalIgnores(["projects/**/*"]), {
-    files: ["**/*.ts"],
-
+export default defineConfig([
+  globalIgnores(['projects/**/*']),
+  {
+    files: ['**/*.ts'],
     extends: compat.extends(
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:@angular-eslint/recommended",
-        "plugin:@angular-eslint/template/process-inline-templates",
-        "plugin:prettier/recommended",
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:@angular-eslint/recommended',
+      'plugin:@angular-eslint/template/process-inline-templates',
+      'plugin:prettier/recommended'
     ),
-
     rules: {
-        "prettier/prettier": ["error", {
-            "endOfLine": "auto",
-        }],
+      'prettier/prettier': 'error',
     },
-}, {
-    files: ["**/*.html"],
-
-    extends: compat.extends(
-        "plugin:@angular-eslint/template/recommended",
-        "plugin:@angular-eslint/template/accessibility",
-    ),
-
-    rules: {},
-}, {
-    files: ["**/*.html"],
-    extends: compat.extends("plugin:@angular-eslint/template/recommended"),
-    rules: {},
-}, {
-    files: ["**/*.html"],
-    ignores: ["**/*inline-template-*.component.html"],
-    extends: compat.extends("plugin:prettier/recommended"),
-
+  },
+  {
+    files: ['**/*.html'],
+    extends: compat.extends('plugin:@angular-eslint/template/recommended', 'plugin:prettier/recommended'),
     rules: {
-        "prettier/prettier": ["error", {
-            parser: "angular",
-            "endOfLine": "auto",
-        }],
+      'prettier/prettier': [
+        'error',
+        {
+          parser: 'angular',
+        },
+      ],
     },
-}]);
+  },
+]);
