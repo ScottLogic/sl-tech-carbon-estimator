2d2b291a1c39339d3264d945978abf7fa63add45
lincent <11511477+lincent@users.noreply.github.com>
2025-12-09 15:47:54 +0000

    feature: add estimate saas service

---
+++ src/app/features/saas/services/estimate-saas-emissions.service.ts
@@ -0,0 +1,14 @@
+import { inject, Injectable } from '@angular/core';
+import { Saas } from '../components/saas.constants';
+import { Microsoft365Service } from './microsoft365.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class EstimateSaasEmissionsService {
+  private microsoft365Service = inject(Microsoft365Service);
+
+  public estimateEmissions(values: Saas): number {
+    return this.microsoft365Service.calculateEmissions(values.microsoft365);
+  }
+}
