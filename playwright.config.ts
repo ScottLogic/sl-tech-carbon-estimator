2808ca48c36ec01d6c2d5bafb728b2bc6dd3fc48
Kieran Ellis <kellis@scottlogic.com>
2025-12-12 11:18:53 +0000

    test: add screenshots on failures

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,16 +1,5 @@
 import { defineConfig, devices } from '@playwright/test';
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
-
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 export default defineConfig({
   testDir: './playwright-tests',
   snapshotDir: './playwright-tests/snapshot-directory',
@@ -29,6 +18,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
   },
   expect: {
     toHaveScreenshot: { maxDiffPixels: 50 },
@@ -40,16 +30,6 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'], viewport: { width: 1920, height: 1200 } },
     },
-
-    // {
-    //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'] },
-    // },
-
-    // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
-    // },
   ],
 
   /* Run your local dev server before starting the tests */
