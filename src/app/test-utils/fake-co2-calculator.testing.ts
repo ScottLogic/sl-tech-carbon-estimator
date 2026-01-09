0ac93441856a8ad086ec4c31e10618b9abffeac7
lincent <11511477+lincent@users.noreply.github.com>
2026-01-09 10:21:25 +0000

    refactor: move facade direcotry to services

--- src/app/test-utils/fake-co2-calculator.testing.ts
+++ src/app/test-utils/fake-co2-calculator.testing.ts
@@ -1,5 +1,5 @@
-import { CO2_CALCULATOR } from '../facades/CO2InjectionToken';
-import { FakeCO2Calculator } from '../facades/FakeCO2Calculator';
+import { CO2_CALCULATOR } from '../services/facades/CO2InjectionToken';
+import { FakeCO2Calculator } from '../services/facades/FakeCO2Calculator';
 
 export const fakeCO2CalculatorFactory = () => {
   const instance = new FakeCO2Calculator();
